a6d41b885fc1bd91ff3e501cf1c7c4dc2aedbfdb
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2018-04-13 01:27:15 +0530

    request parsing

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
+def __fetch(url="https://ambitionbox.com"):
+    r = requests.get(url)
+    return r.text
+
 @app.route("/")
 def index():
-    return "Hello world"
+    return __fetch()
