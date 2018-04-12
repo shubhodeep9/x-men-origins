4477d5348f6665dada2ca481e376424dc0ec5b55
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2018-04-13 02:02:15 +0530

    handling errors

--- web/__init__.py
+++ web/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_cors import CORS
 import requests
 
@@ -6,10 +6,24 @@ app = Flask(__name__)
 CORS(app)
 
 def __fetch(url="https://ambitionbox.com"):
-    r = requests.get(url)
-    return r.text
+    try:
+        r = requests.get(url)
+        return {
+                "status": r.status_code,
+                "body": r.text
+            }
+    except:
+        return {
+                "status": 500,
+                "body": ""
+                }
+
 
 @app.route("/")
 def index():
     url = request.args.get('url')
-    return __fetch(url)
+    r = __fetch(url)
+    if(r["status"] != 200):
+        abort(r["status"])
+
+    return r["body"]
